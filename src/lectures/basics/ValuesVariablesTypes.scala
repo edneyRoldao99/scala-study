0eb86d3643869effd148fbbae61ec3e3cdcc2d39
edneyRoldao99 <edney.souza_v@99app.com>
2020-04-08 12:49:56 -0300

    variables and types

---
+++ src/lectures/basics/ValuesVariablesTypes.scala
@@ -0,0 +1,28 @@
+package lectures.basics
+
+object ValuesVariablesTypes extends App {
+
+  // VAL is immutable
+  // VAR can be resign
+  // the compiler can infer types
+  // semicolon is not required
+
+  // creating variables
+  val numero1: Int = 10
+  val numero2 = 10 // types are optional
+
+  val text1: String = "hello my friend"
+
+  val boo1: Boolean = true
+
+  val char1: Char = 'a'
+  val int1: Int = numero1
+  val short1: Short = 123;
+  val long1: Long = 10L
+  val float1: Float = 45.50F
+
+  // var is mutable
+  var nummero3: Int = 10
+  nummero3 = 30
+
+}
